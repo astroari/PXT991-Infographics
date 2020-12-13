72b62799fe838dc340c07f000bf65186e9b1db95
Arina <bagdasaryana@cardiff.ac.uk>
2020-12-13 16:49:54 +0000

    Change text

--- interactive-waterfall.py
+++ interactive-waterfall.py
@@ -4,6 +4,7 @@ import datetime as dt
 import dash             
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 import plotly          
@@ -14,6 +15,7 @@ df = pd.read_csv("testdata1-m.csv")
 
 app = dash.Dash(__name__)
 
+
 app.layout = html.Div([
     html.Div([
             html.Pre(children= "Radiative forcing Infographic by Arina, Kavian and Mateusz",
@@ -23,7 +25,7 @@ app.layout = html.Div([
         html.H1("Radiative Forcing")
         ]),
     html.Div([
-        html.P("The term 'Radiative Forcing' refers simply to the difference of solar irradiance absorbed by the earth, compared to how much is radiated back to space. This way a system in thermal equilibrium would have zero radiative forcing. Where solar irradiance is the amount of sun’s energy received per unit area per second with units of Watts per meter squared. A positive radiative forcing means more energy is absorbed by the Earth than reflected back into space, which results in the Earth getting warmer and vice versa. Here we break down radiative forcing into its components often refered to as 'forcing agents', categorising it by the effects responsible.", className="intro"),
+        html.P("The term 'Radiative Forcing' refers simply to the difference of solar irradiance absorbed by the Earth, compared to how much is radiated back to space. This way a system in thermal equilibrium would have zero radiative forcing. Where solar irradiance is the amount of sun’s energy received per unit area per second with units of Watts per meter squared. A positive radiative forcing means more energy is absorbed by the Earth than reflected back into space, which results in the Earth getting warmer and vice versa. Here we break down radiative forcing into its components often refered to as 'forcing agents', categorising it by the effects responsible.", className="intro"),
         html.P(["Some of the agents are ", html.Span("natural ", style={'color': '#3FA0A0'}), "while others originate from ", html.Span("human ", style={'color': '#bc99e0'}), "activity."], className="agents"),
 
     ]),
